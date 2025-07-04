8adf75f7520ec1f456f1c43976e0c69844d96505
Nancy Quinonez <nancyq29@hotmail.com>
2025-07-04 17:53:29 -0500

    Completed events api routes

--- api/events.js
+++ api/events.js
@@ -7,6 +7,7 @@ import {getEvents, getEventById, createEvent, updateEvent, deleteEvent} from '..
 router.get("/", async (req, res, next) => {
     try{
         const events = await getEvents();
+
         return res.send(events);
         // return res.json(events):
     }catch(err){
@@ -14,3 +15,86 @@ router.get("/", async (req, res, next) => {
         next(err);
     }
 });
+
+router.get("/:id", async (req, res, next) => {
+    try{
+        const id = Number(req.params.id);
+        if(isNaN(id) || id < 0){
+            return res.status(400).send({error: "Please provide a valid event"});
+        }
+
+        const event = await getEventById(id);
+        if(!event){
+            return res.status(404).send({error: "Event does not exist"});
+        }
+
+        res.send(event);
+    }catch(err){
+        console.error("Error fetching event by ID:", err);
+        next(err);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try{
+        const {username, body, user_id} = req.body;
+        if(!username || !body || !user_id){
+            return res.status(400).send({error: "Missing required fields"});
+        }
+
+        const event = await createEvent({username, body, user_id});
+        return res.status(201).send(event);
+
+    }catch(err){
+        console.error("Error creating event:", err);
+        next(err);
+    }
+});
+
+router.put("/:id", async (req, res, next) => {
+    try{
+        const id = Number(req.params.id);
+
+        if(!Number.isInteger(id) || id < 0){
+            return res.status(400).send({error: "Please provide a valid ID"});
+        }
+
+        const {username, body, user_id} = req.body;
+        if(!username || !body || !user_id){
+            return res.status(400).send({error: "Missing required fields"});
+        }
+
+        const event = await getEventById(id);
+        if(!event){
+            return res.status(404).send({error: "Event does not exist"});
+        }
+
+        const updated = await updateEvent({id, username, body, user_id});
+        res.status(200).send(updated);
+
+    }catch(err){
+        console.error("Error updating event:", err);
+        next(err);
+    }
+});
+
+router.delete("/:id", async (req, res, next) => {
+    try{
+        const id = Number(req.params.id);
+        if(!Number.isInteger(id) || id < 0){
+            return res.status(400).send({error: "Please provide a vaild event"});
+        }
+
+        const event = await getEventById(id);
+        if(!event){
+            return res.status(404).send({error: "Event not found"});
+        }
+
+        await deleteEvent(id);
+        return res.sendStatus(204);
+
+    }catch(err){
+        console.error("Error deleting event:", err);
+        next(err);
+    }
+})
