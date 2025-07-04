6c0649b15496f91de2d890a9e4a77592508ab04b
Lyla Lynn <lyladlynn@gmail.com>
2025-07-04 15:10:58 -0500

    finally got the finalSeed.js to seed to the database 🥹

--- db/client.js
+++ db/client.js
@@ -1,3 +1,15 @@
 import pg from 'pg';
-const db = new pg.Client(process.env.DATABASE_URL);
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Pool } = pg;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("❌ DATABASE_URL is not defined in .env");
+}
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
 export default db;
--- db/seed/eventResourcesSeed.js
+++ db/seed/eventResourcesSeed.js
@@ -2,17 +2,20 @@ import db from '../client.js';
 import { createEvent } from '../queries/events.js';
 import { createResource } from '../queries/resources.js';
 
-await db.connect();
-await seedEvents();
-await seedResources();
-
 export async function seedEvents() {
-    await createEvent({username: "itsnancyq", body: "Craft and Connect: Join us virtually on July 24, 2025 at 7pm(CST) to celebrate our wins and creating fun crafts! More details to come!", user_id: 1})
+    try {
+        await createEvent({username: "itsnancyq", body: "Craft and Connect: Join us virtually on July 24, 2025 at 7pm(CST) to celebrate our wins and creating fun crafts! More details to come!", user_id: 1});
+    } catch (error) {
+        console.error('❌ Error creating event:', error)
+    }
 }
 
 export async function seedResources() {
-    await createResource({title: "988 Suicide and Crisis Lifeline", body: "Help is available 24/7. Dial 988.", user_id: 1})
+    try {
+        await createResource({title: "988 Suicide and Crisis Lifeline", body: "Help is available 24/7. Dial 988.", user_id: 1});
+    } catch (error) {
+        console.error("❌ Error creating resource:", error)
+    }
 }
 
-await db.end();
-console.log("🌱 Database seeded.");
+console.log("🌱 Events & Resources seeded.");
