8a40b0877696981db11b27cdeb788ac6a293f652
Lyla Lynn <lyladlynn@gmail.com>
2025-07-03 20:48:05 -0500

    importing seed files to finalSeed.js and shcema files to finalSchema.sql

--- db/seed/eventResourcesSeed.js
+++ db/seed/eventResourcesSeed.js
@@ -6,10 +6,10 @@ await seedResources();
 await db.end();
 console.log("🌱 Database seeded.");
 
-async function seedEvents() {
+export async function seedEvents() {
     await createEvent({username: "itsnancyq", body: "Craft and Connect: Join us virtually on July 24, 2025 at 7pm(CST) to celebrate our wins and creating fun crafts! More details to come!", user_id: 1})
 }
 
-async function seedResources() {
+export async function seedResources() {
     await createResource({title: "988 Suicide and Crisis Lifeline", body: "Help is available 24/7. Dial 988.", user_id: 1})
 }
