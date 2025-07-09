b715501dec70a9038fc1884faa08253b78168def
Lyla Lynn <lyladlynn@gmail.com>
2025-07-09 16:15:31 -0500

    fixed router.put by updating to router.patch and updating updateUser query with if statements for undefined values.

--- auth/me.js
+++ auth/me.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
 import verifyToken from './middleware/verifyToken.js';
 import { deleteUser, getUserById, updateUser } from '../db/queries/usersQueries.js';
 const router = express.Router();
@@ -13,14 +15,12 @@ router.get('/:id', verifyToken, async ( req, res, next ) => {
         }
         res.json(user);
     } catch (error) {
-        console.error('❌ Error in /auth/account/:id route:', error.message);
+        console.error('❌ Error in GET /auth/account/:id route:', error.message);
         res.status(500).json({ message: 'Failed to get user by ID.' });
     }
 })
 
-import validator from 'validator';
-
-router.put('/:id', verifyToken, async (req, res, next) => {
+router.patch('/:id', verifyToken, async (req, res, next) => {
   const { id } = req.params;
   const { first_name, last_name, email, username, password } = req.body;
   try {
@@ -56,12 +56,13 @@ router.put('/:id', verifyToken, async (req, res, next) => {
       email,
       password: hashedPassword,
     });
+    console.log(updatedUser);
     if (!updatedUser) {
       return res.status(404).json({ message: 'User not found' });
     }
     res.json(updatedUser);
   } catch (error) {
-    console.error('❌ Error in /auth/account/:id route:', error.message);
+    console.error('❌ Error in PATCH /auth/account/:id route:', error.message);
     res.status(500).json({ message: 'Failed to update user.' });
   }
 });
@@ -79,37 +80,5 @@ router.delete('/:id', verifyToken, async (req, res, next) => {
     return res.status(500).json({ message: 'Failed to delete user.' });
   }
 });
-
-// router.put('/:id', verifyToken, async ( req, res, next ) => {
-//     try {
-//         const { first_name, last_name, email, username, password } = req.body;
-//         const { id } = req.params;
-//         if ( !username || !password ) {
-//             return res.status(400).json({ message: 'Username and password required.' });
-//         }
-//         const hashedPassword = await bcrypt.hash(password, 10);
-//         const updatedUser = await updateUser(id, username, hashedPassword);
-//         if (!updatedUser) {
-//             return res.status(404).json({ message: 'User not found' });
-//         }
-//         res.json(updatedUser);
-//     } catch (error) {
-//         console.error('❌ Error in /auth/account/:id route:', error.message);
-//         res.status(500).json({ message: 'Failed to update user.' });
-//     }
-// });
-
-// router.delete('/:id', verifyToken, async ( req, res, next ) => {
-//     try {
-//         const deletedUser = await deleteUser(req.params.id);
-//         if ( !deletedUser ) {
-//             return res.status(404).json({ message: 'User not found.' });
-//         }
-//         res.json({ message: 'User deleted successfully.' });
-//     } catch (error) {
-//         console.error('❌ Error in /auth/account/:id route:', error);
-//         res.status(500).json({ message: 'Failed to delete user.' });
-//     }
-// });
 
 export default router;
