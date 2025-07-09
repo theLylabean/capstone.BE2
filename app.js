085429f568548095b0b24261f6f03458d4825851
Lyla Lynn <lyladlynn@gmail.com>
2025-07-09 16:26:52 -0500

    adding followRouter to app.js and now testing follows routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import commentsRouter from "./routes/comments.js";
 import authRouter from './auth/index.js';
 import eventsRouter from './api/events.js';
 import resourcesRouter from './api/resources.js';
+import followRouter from './api/follows.js'
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/auth", authRouter);
+app.use('/api/follows', followRouter);
 app.use("/api/posts", postsRouter);
 app.use('/api/comments', commentsRouter);
 app.use('/api/events', eventsRouter);
