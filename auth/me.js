b03bd5ef7518b696a0b88ae75e4ecf6fd1525b01
Lyla Lynn <lyladlynn@gmail.com>
2025-07-08 20:23:57 -0500

    updating routes. taking out console.logs

--- auth/me.js
+++ auth/me.js
@@ -4,10 +4,9 @@ import { deleteUser, getUserById, updateUser } from '../db/queries/usersQueries.
 const router = express.Router();
 
 router.get('/:id', verifyToken, async ( req, res, next ) => {
-    console.log('✅ GET /auth/account/:id route hit');
-    console.log('req.params.id:', req.params.id);
     try {
-        const user = await getUserById(req.params.id);
+        const { id } = req.params.id;
+        const user = await getUserById(id);
         console.log('User from DB:', user);
         if (!user) {
             return res.status(404).json({ message: 'User not found.' });
@@ -22,11 +21,12 @@ router.get('/:id', verifyToken, async ( req, res, next ) => {
 router.put('/:id', verifyToken, async ( req, res, next ) => {
     try {
         const { username, password } = req.body;
+        const { id } = req.params.id;
         if ( !username || !password ) {
             return res.status(400).json({ message: 'Username and password required.' });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
-        const updatedUser = await updateUser(req.params.id, username, hashedPassword);
+        const updatedUser = await updateUser(id, username, hashedPassword);
         if (!updatedUser) {
             return res.status(404).json({ message: 'User not found' });
         }
@@ -52,19 +52,13 @@ router.put('/:id', verifyToken, async ( req, res, next ) => {
 
 router.delete('/:id', verifyToken, async (req, res, next) => {
   try {
-    const deletedUser = await deleteUser(req.params.id);
-    console.log('🧪 Deleted user:', deletedUser);
-
+    const { id } = req.params.id;
+    const deletedUser = await deleteUser(id);
     if (!deletedUser) {
-      console.log('🧪 No user found to delete');
       return res.status(404).json({ message: 'User not found.' });
     }
-
-    console.log('✅ Sending success response...');
     return res.json({ message: 'User deleted successfully.' });
-
   } catch (error) {
-    console.error('❌ Error in DELETE /auth/account/:id:', error.message);
     return res.status(500).json({ message: 'Failed to delete user.' });
   }
 });
